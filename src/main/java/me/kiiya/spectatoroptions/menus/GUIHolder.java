5c31b6c149f944b5c8b2ce13ec0c31c45150508f
Minecraft0122 <168195378+Minecraft0122@users.noreply.github.com>
2026-03-22 15:11:08 +0800

    Delete src/main directory

--- src/main/java/me/kiiya/spectatoroptions/menus/GUIHolder.java
+++
@@ -1,8 +0,0 @@
-package me.kiiya.spectatoroptions.menus;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.InventoryHolder;
-
-public interface GUIHolder extends InventoryHolder {
-    void onClick(InventoryClickEvent e);
-}
--- src/main/java/me/kiiya/spectatoroptions/support/Support.java
+++
@@ -1,5 +0,0 @@
-package me.kiiya.spectatoroptions.support;
-
-public enum Support {
-    BEDWARS1058, BEDWARS2023
-}
